e6ff6ff1cf2b701880e4029fe4c0619ab80d5e7f
chenweilian0419 <73155397+chenweilian0419@users.noreply.github.com>
2025-06-26 14:20:51 +0800

    Update attractor.js

--- attractor.js
+++ attractor.js
@@ -1,137 +1,121 @@
-let balls = [];
-// Length of the trail
-let trailLength = 35;
-// Cooldown of interacting to prevent adding or deleting too fast due to rapid clicks
-let cooldown = 0;
-// Gravity constant to control the strength of gravity
-let gravity = 1;
-// A slider to control the value of gravity
-let gravStrength;
-let showVelocity = 1;
-// Position of Centre
-let pivot;
-// A slider to control the size of the field (Or actually changing the size and position of the balls)
-let zoom;
-
-function setup() {
-  createCanvas(600, 600);
-
-  // Initialising the Sliders and Buttons
-  zoom = createSlider(0.3, 1.5, 1, 0.05);
-  zoom.position(120, 10);
-  zoom.size(80);
-  gravStrength = createSlider(0.5, 5, 1, 0.1);
-  gravStrength.position(370, 10);
-  gravStrength.size(100);
-  velocityButton = createButton('no arrow(v)');
-  velocityButton.position(490, 10);
-  velocityButton.mousePressed(toggleVelocity);
-  
-  textSize(19);
-  textAlign(LEFT, TOP);
-  
-  // Initialise the pivot for translating around
-  pivot = createVector(0, 0);
-}
-
-function draw() {
-  // Changing gravity to the current strength value
-  gravity = gravStrength.value();
-  background(35);
-  resetMatrix()
-  // Putting description texts
-  fill("white")
-  text("zoom(scroll): ", 10, 10);
-  text("gravity strength: ", 230, 10);
-
-  // Translating and scaling
-  handleTranslation();
-  // Translate to the centre to zoom and draw zoom pivot cross in the centre
-  translate(width/2, height/2);
-  // Draws the zoom pivot for reference
-  stroke(250, 100, 0, 200)
-  scale(zoom.value());
-  strokeWeight(3)
-  line(0, -10, 0, 10);
-  line(-10, 0, 10, 0);
-  strokeWeight(0);
-  // Actually translate to the centre we want
-  translate(pivot.x, pivot.y);
-  // Update the position of balls
-  balls.forEach((p) => {
-    p.update();
-    //p.checkEdges();
-  });
-  // Applies gravitational force
-  for (let i = 0; i < balls.length; i++) {
-    balls.forEach((p) => {
-      let force = p.calculateAttraction(balls[i]);
-      balls[i].applyForce(force);
-    });
+class Attractor {
+  constructor(x, y, m) {
+    // Initiating
+    this.position = createVector(x, y);
+    // We take the area of a ball as its mass
+    // Although area = pi * r^2, we just ignore pi here for simplicity
+    this.mass = m;
+    this.r = sqrt(m);
+    // Random initial velocity
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
+    this.acceleration = createVector(0, 0);
+    this.col = color(random(100, 250), random(100, 250), random(100, 250));
+    // A list to store previous positions of the ball which makes up the trail
+    this.trail = [];
   }
-  for (let i = 0; i < balls.length; i++) {
-    for (let j = i + 1; j < balls.length; j++) {
-      // Determine distance between the balls and check if they collide
-      if (p5.Vector.dist(balls[i].position, balls[j].position) < (balls[i].r + balls[j].r)) {
-        // The balls collide and hence adjust one's mass and delete the other
-        balls[i].collide(balls[j]);
-        balls.splice(j, 1);
-      }
+
+  calculateAttraction(object) {
+    let force = p5.Vector.sub(this.position, object.position);
+    let distance = force.mag();
+    // Limiting the distance to eliminate "extreme" results
+    // for very close or very far objects
+    distance = constrain(distance, 10, 500);
+    force.normalize();
+    // Calculate gravitional force magnitude
+    let strength = (gravity * this.mass * object.mass) / (distance * distance);
+    // Get force vector --> magnitude * direction
+    force.mult(strength);
+    return force;
+  }
+  
+  applyForce(force) {
+    // a = F / m
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  }
+  
+  update() {
+    // Add current position to trail
+    this.trail.push(this.position.copy());
+    if (this.trail.length>trailLength){
+      strokeWeight(0.5);
+      this.trail.shift();
+    }
+    // Simulates motion
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+  
+  display() {
+    strokeWeight(0);
+    for (let i = 0; i < this.trail.length; i++) {
+      // Trail will increasing transparency and size
+      this.col.setAlpha(i)
+      fill(this.col);
+      circle(this.trail[i].x, this.trail[i].y, this.r * 2 / (trailLength+10) * (i+10));
+    }
+    strokeWeight(2);
+    stroke(0);
+    this.col.setAlpha(255);
+    fill(this.col);
+    circle(this.position.x, this.position.y, this.r * 2);
+    // Drawing velocity arrows
+    if(showVelocity) {
+      this.drawArrow(this.velocity.copy(), 'red');
     }
   }
-  balls.forEach((p) => {
-    p.display();
-  });
+  
+  drawArrow(vec, col) {
+    // Store the current status to not affect other places
+    push();
+    stroke(col)
+    fill(col)
+    // Constraining magnitude to prevent extreme values like an arrow with length 400
+    vec.setMag(vec.mag() + 0.1);
+    vec.mult(this.r)
+    vec.setMag(constrain(vec.mag(), 10, 100))
+    strokeWeight(2);
+    translate(this.position);
+    line(0, 0, vec.x, vec.y);
 
-  // Decrease cooldown value
-  cooldown--;
-}
-
-function mouseClicked() {
-  if (cooldown <= 0) {
-    // Finding the actual mouseX after removing effect of translating and scaling.
-    let mX = (mouseX - width/2) / zoom.value() - pivot.x,
-        mY = (mouseY - height/2) / zoom.value() - pivot.y;
-    for (let i = 0; i < balls.length; i++) {
-      // Determine whether the mouse is inside the ball
-      if (p5.Vector.dist(balls[i].position, createVector(mX, mY)) < balls[i].r) {
-        // Delete the ball
-        balls.splice(i, 1);
-        return;
-      }
+    let arrowSize = this.r / 4;
+    // Maths out the arrowhead
+    let angle = atan2(vec.y, vec.x);
+    translate(vec.x, vec.y);
+    rotate(angle);
+    triangle(0, 0, -arrowSize, arrowSize / 2, -arrowSize, -arrowSize / 2);
+    pop();
+    // Reset status
+  }
+  
+  collide(object) {
+    // Merging velocity based on mass
+    let xVel = (this.mass*this.velocity.x + object.mass*object.velocity.x) / (this.mass+object.mass);
+    let yVel = (this.mass*this.velocity.y + object.mass*object.velocity.y) / (this.mass+object.mass);
+    this.velocity.x = xVel;
+    this.velocity.y = yVel;
+    
+    //
+    this.mass += object.mass;
+    this.r = sqrt(this.mass);
+  }
+  
+  // Check if the ball is bouncing at the edge(previous idea that is unused)
+  checkEdges() {
+    if (this.position.x + this.r > width) {
+      this.velocity.x *= -1;
+      this.position.x = width - this.r;
+    } else if (this.position.x - this.r < 0) {
+      this.velocity.x *= -1;
+      this.position.x = this.r;
     }
-    // Add a new ball if no ball is deleted
-    balls.push(new Attractor(mX, mY, random(30, 80)));
-    cooldown = 20;
+    if (this.position.y + this.r > height) {
+      this.velocity.y *= -1;
+      this.position.y = height - this.r;
+    } else if(this.position.y - this.r < 0) {
+      this.velocity.y *= -1;
+      this.position.y = this.r;
+    }
   }
-}
-
-function handleTranslation() {
-  let step = 5 / zoom.value(); // Scale movement so it's consistent at different zooms
-  // The directions are opposite because you are moving background
-  if (keyIsDown(LEFT_ARROW) || keyIsDown(65))  pivot.x += step;
-  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) pivot.x -= step;
-  if (keyIsDown(UP_ARROW) || keyIsDown(87))    pivot.y += step;
-  if (keyIsDown(DOWN_ARROW) || keyIsDown(83))  pivot.y -= step;
-}
-
-function keyPressed() {
-  if (key == 'v') {
-    toggleVelocity();
-  }
-}
-
-
-// Toggles showVelocity
-function toggleVelocity() {
-  showVelocity ^= 1;
-  velocityButton.html(showVelocity == 0 ? 'show arrow(v)' : 'no arrow(v)');
-}
-
-function mouseWheel(event) {
-  // Allows mouse wheel to control the zoom slider
-  let delta = -event.delta / 2000;
-  let current = zoom.value();
-  zoom.value(constrain(current + delta, zoom.elt.min, zoom.elt.max));
-  return false;
 }
